8d8b8b45fe9482a5b046415e4c3a01f6cc4dce2d
314ronin <pownmymail@gmail.com>
2019-10-06 03:56:21 +0300

    Initial project Add

---
+++ src/main/java/com/shlang/springpetclinic/SpringPetClinicApplication.java
@@ -0,0 +1,15 @@
+package com.shlang.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+
+@SpringBootApplication
+public class SpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicApplication.class, args);
+    }
+
+}

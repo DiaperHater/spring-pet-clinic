6436654418643b46bcf566ed3c56e21fe96cc6ca
314ronin <pownmymail@gmail.com>
2019-11-04 00:38:06 +0300

    Map based services are implemented. closes #6.

--- pet-clinic-data/src/main/java/com/shlang/springpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/com/shlang/springpetclinic/services/CrudService.java
@@ -5,9 +5,9 @@ import java.util.Set;
 
 public interface CrudService<T, ID> {
 
-    Set<T> getAll();
+    Set<T> findAll();
 
-    T getById(ID id);
+    T findById(ID id);
 
     T save(T object);
 
